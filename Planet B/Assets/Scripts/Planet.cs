8158a3391ee4c1883088d4e22d2e1021e44df7ac
5viki <viktor.5rovic@gmail.com>
2025-03-17 20:59:36 +0100

    Set up unity for Git. Imported first assets.

---
+++ Planet B/Assets/Scripts/Planet.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] public Color outlineColor;
+    [SerializeField] public Color baseColor;
+    [SerializeField] public Color insideColor;
+}
